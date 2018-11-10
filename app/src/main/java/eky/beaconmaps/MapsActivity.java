bc09dfc2e83ebde2ac07fcf11af96a5a6ec67578
Enes Kamil YILMAZ <enesky07@gmail.com>
2018-11-10 10:21:59 +0300

    Update EstimoteSdk added. GoogleMap api key cover is done.

--- app/src/main/java/eky/beaconmaps/MapsActivity.java
+++ app/src/main/java/eky/beaconmaps/MapsActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.estimote.coresdk.common.config.EstimoteSDK;
+import com.estimote.coresdk.recognition.utils.EstimoteBeacons;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -24,11 +27,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //TODO: get your AppId and AppToken you need to create new application in Estimote Cloud.
+        //EstimoteSDK.initialize(applicationContext, appId, appToken);
+        // Optional, debug logging.
+        EstimoteSDK.enableDebugLogging(true);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+       // mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        googleMap.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                        this, R.raw.style_json));
+        //mMap.setZoomControlsEnabled(true);
+        //mMap.setCompassEnabled(true);
+
+        //TODO find how to show present location
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
