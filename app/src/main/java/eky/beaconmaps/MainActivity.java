81bdccf7462b06b73da7ba782690d86bd29fd83f
ENES KAMIL YILMAZ (Bt Strateji ve Yonetisim Bsk. - Memur/Pt) <ENES.YILMAZ@THY.COM>
2019-03-16 00:16:46 +0300

    Font added. Font library -Calligrapher- added.

--- app/src/main/java/eky/beaconmaps/MainActivity.java
+++ app/src/main/java/eky/beaconmaps/MainActivity.java
@@ -3,11 +3,18 @@ package eky.beaconmaps;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import me.anwarshahriar.calligrapher.Calligrapher;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Call this on every view ... i guess
+        Calligrapher calligrapher = new Calligrapher(this);
+        calligrapher.setFont(this, "font/MuseoSans.otf", true);
+
     }
 }
