84bac406e700113a803a0e7655739a63715e266a
ENES KAMIL YILMAZ (Bt Strateji ve Yonetisim Bsk. - Memur/Pt) <ENES.YILMAZ@THY.COM>
2019-03-17 23:28:43 +0300

    Updated Login screen's design. Added libraries. App made only portrait mode.

--- app/src/main/java/eky/beaconmaps/activities/LoginActivity.java
+++ app/src/main/java/eky/beaconmaps/activities/LoginActivity.java
@@ -1,39 +1,32 @@
 package eky.beaconmaps.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import eky.beaconmaps.R;
 import me.anwarshahriar.calligrapher.Calligrapher;
 
-public class SignInActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
+        setContentView(R.layout.activity_login);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        TextView toolbarTitle = toolbar.findViewById(R.id.tv_toolbar_title);
-        toolbarTitle.setText(R.string.title_signin_activity);
-        setSupportActionBar(toolbar);
-
-        //Call this on every view ... i guess
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         Calligrapher calligrapher = new Calligrapher(this);
         calligrapher.setFont(this, "font/MuseoSans.otf", true);
 
-        MaterialButton button = findViewById(R.id.button);
+        MaterialButton button = findViewById(R.id.button_login);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/eky/beaconmaps/activities/MainActivity.java
+++ app/src/main/java/eky/beaconmaps/activities/MainActivity.java
@@ -1,9 +1,8 @@
 package eky.beaconmaps.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import eky.beaconmaps.R;
 import me.anwarshahriar.calligrapher.Calligrapher;
@@ -16,12 +15,8 @@ public class MainActivity extends AppCompatActivity {
         getIntent();
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        TextView toolbarTitle = toolbar.findViewById(R.id.tv_toolbar_title);
-        toolbarTitle.setText(R.string.title_main_activity);
-        setSupportActionBar(toolbar);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
 
-        //Call this on every view ... i guess
         Calligrapher calligrapher = new Calligrapher(this);
         calligrapher.setFont(this, "font/MuseoSans.otf", true);
 
